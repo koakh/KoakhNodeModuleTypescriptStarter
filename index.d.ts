0e004bc465898f455b0c25c45a3d5a82088c025e
mariomonteiro <mario.monteiro@critical-links.com>
2020-09-02 17:43:00 +0100

    fixed ar.json

---
+++ index.d.ts
@@ -0,0 +1,6 @@
+// Type definitions for typescript-rest-actions-api 1.0.0
+// Project: Typescript Rest Actions Api
+// Definitions by: Mário Monteiro https://koakh.com
+// [module.d.ts](https://www.typescriptlang.org/docs/handbook/declaration-files/templates/module-d-ts.html)
+
+export *  from './src/index'
